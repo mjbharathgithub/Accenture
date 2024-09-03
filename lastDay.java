74707137f35addb60d27e276f7d78ae2d20a24a3
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-09-03 10:55:31 +0530

    Update lastDay.java

--- lastDay.java
+++ lastDay.java
@@ -105,4 +105,140 @@ public class Main
         System.out.println(lcmOfTwoNum(12,9));
         
 	}
+
+	/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+    static double formulae(double h, double v,double vn) {
+ double en = v/vn;
+ double hn = h * (Math.pow(en,2));
+ System.out.println(en+" "+hn);
+ return hn;
+}
+
+     public static String fun(int n) {
+         StringBuilder res = new StringBuilder();
+         while (n > 0) {
+         if ((n % 10) % 2 == 0) {
+         res.insert(0, "Even");
+         } else {
+         res.insert(0, "Odd");
+         }
+         n /= 10;
+         }
+         return res.toString();
+ }
+ 
+    static int roundOff(int l, int b) {
+         int res = (int)Math.round((3.14 * l * b));
+         return res;
+}
+
+    static int SplFnb(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+
+    int prev2 = 0;  // Equivalent to SplFnb(n-2)
+    int prev1 = 1;  // Equivalent to SplFnb(n-1)
+    int result = 0;
+
+    for (int i = 2; i <= n; i++) {
+        result = prev1 * prev1 + prev2 * prev2;
+        prev2 = prev1;
+        prev1 = result;
+    }
+
+    return result;
+}
+
+static int fibonacci(int n){
+        if(n<1) return -1;
+        if(n<3) return n-1;
+        int fib=0,one=0,two=1;
+        for(int i=2;i<n;i++){
+            
+            fib=one*one+two*two;
+            System.out.print(fib+ " ");
+            one=two;
+            two=fib;
+            
+        }
+        
+        return fib;
+        
+    }
+    public static int fun(String s) {
+         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+         int cnt = 0;
+         int fact = 1;
+         for (int i = 0; i < s.length(); i++) {
+         char c = s.charAt(i);
+         boolean isVowel = false;
+         for (char vowel : vowels) {
+         if (c == vowel) {
+         isVowel = true;
+         break;
+         }
+         }
+         if (!isVowel && c != ' ') {
+         cnt++;
+         fact *= cnt;
+         }
+         }
+         return fact;
+ }
+ 
+    static int FileHandling(String[] files) {
+         try {
+         int val = 0;
+         for(String s : files) {
+         val = Math.max(Integer.valueOf(s.replace("file_","")),val);
+         }
+         return val;
+         }
+         catch(Exception e) {
+         return -1;
+         }
+        
+    }
+    
+    //16 2 10    50 48 7 
+    
+    static int numOfChocalteBox(int chockies,int limit,int day){
+        int bought=1,stock=chockies;
+        for(int d=1;d<=day;d++){
+            
+            stock-=limit;
+            if(stock<limit&&d%7==0) return -1;
+            
+            else if(stock<limit){
+                stock+=chockies;
+                bought++;
+            }
+            
+            
+        }
+        return bought;
+    }
+// 	public static void main(String[] args) {
+// // 		System.out.println(roundOff(45,56));
+// // 		int a=(int)Math.sqrt(565);
+//     // 	System.out.println(fibonacci(6));
+//     // 		System.out.println(SplFnb(5));
+    
+// //         System.out.println(FileHandling(new String[]{"file_1", "file_20", 
+// // "file_3", "file_10"}));
+//         System.out.println(numOfChocalteBox(16,2,10));
+        
+		
+// 	}
+// }
 }
