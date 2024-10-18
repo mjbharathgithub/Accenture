23317de71eff61151525d19419889dc6ed327f5a
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-10-18 11:01:44 +0530

    Update ArrayBasedProblems.java

--- ArrayBasedProblems.java
+++ ArrayBasedProblems.java
@@ -13,3 +13,20 @@ static int equillibiriunIndex(int arr[],int n){
         
         return -1;
     }
+
+static void rotateRightArrayKTimes(int arr[],int k){
+        int len=arr.length,last;
+        System.out.println("Before : "+Arrays.toString(arr));
+        for(int i=1;i<=k;i++){
+            last=arr[len-1];
+            for(int j=len-1;j>=1;j--){
+                arr[j]=arr[j-1];
+            }
+            arr[0]=last;
+        }
+        
+        System.out.println("After : "+Arrays.toString(arr));
+        
+    }
+
+
